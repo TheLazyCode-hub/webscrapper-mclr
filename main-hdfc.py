7fa803fa0fa032a11b5f0c335da6a7637ea0ca4d
TheLazyCode-hub <gauravshukla052@gmail.com>
2023-02-17 17:10:29 +0530

    hdfc added

--- main-hdfc.py
+++ main-hdfc.py
@@ -6,7 +6,7 @@ HEADERS = {
     'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/110.0.0.0 Safari/537.36'
 }
 
-url = "https://sbi.co.in/web/interest-rates/interest-rates/mclr"
+url = "https://www.hdfcbank.com/personal/resources/rates"
 
 r = requests.get(url, headers=HEADERS)
 
@@ -15,18 +15,22 @@ data = []
 
 soup=BeautifulSoup(r.text,"html.parser") 
 # print(soup.encode("utf-8"))
-# print(soup)
-table = soup.find_all('table')[0].find("tr")
 
-# print(table)
+# id = document.querySelectorAll("[data-pid='widget-hdfc-invest-advisory-9174617'] table")[0].find('tr')
+div = soup.find_all('div', attrs={"data-pid":"widget-hdfc-invest-advisory-9174617"})[0]
 
-for items in table:
+table = div.find("table")
+# print(table.find("tr"))
+tr = table.find("tr")
+
+for items in tr:
     try:
         list_header.append(items.get_text())
     except:
         continue
 
-HTML_data = soup.find_all("table")[0].find_all("tr")[1:]
+HTML_data = table.find_all("tr")[1:]
+
 for element in HTML_data:
     sub_data = []
     for sub_element in element:
@@ -37,4 +41,4 @@ for element in HTML_data:
     data.append(sub_data)
 
 dataFrame = pd.DataFrame(data = data, columns = list_header)
-dataFrame.to_csv('sbi.csv')
+dataFrame.to_csv('hdfc.csv')
